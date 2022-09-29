7d740547049291b30965d68b50e39a12532c5089
Anna Kayurova <anna31k@yahoo.ca>
2022-09-28 23:17:33 -0400

    Exporter la classe pour une utilisation ultérieure

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React from 'react';
 
-function App() {
+
+class App extends React.Component {
+  render(){
+  return(
+    <div className="container">
+      Bonjour au monde de React!
+    </div>
+  );
+  }
+}
+/*function App() {
   return (
     <div className="container">
       Bonjour au monde de React!
     </div>
   );
-}
+}*/
 
 export default App;
