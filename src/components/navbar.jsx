7b03a21934b8ce4867166c234bc9618b603f2192
Anna Kayurova <anna31k@yahoo.ca>
2022-09-29 21:24:19 -0400

    Implantation de la navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 class Navbar extends React.Component{
     render(){
         return(
-            <div className="container">
-                Navbar s'affiche
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+            <div className="navbar-brand">
+                Bienvenue
             </div>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <Link to="/admin" className="nav-link">Admin</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/accueil" className="nav-link">Accueil</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         )
     }
 }
 
-export default Navbar
+export default Navbar;
